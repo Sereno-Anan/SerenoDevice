9359006aeea9aaf791daefe87d42bb42ed814aea
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-11-24 10:16:31 +0900

    サンプルコードを追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
-#include <Arduino.h>
+#include <M5StickC.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  M5.begin();
+  M5.Lcd.fillScreen(WHITE);
+  M5.Lcd.print("Hello World!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  M5.update();
 }
