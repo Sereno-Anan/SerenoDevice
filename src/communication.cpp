004c3ebc7e37c38b6a8eeeeccea9f3b44d664eb8
KubotaShunsuke <s.kubota0921@gmail.com>
2021-12-02 01:11:15 +0900

    POST通信:形はできた #3

--- src/communication.cpp
+++ src/communication.cpp
@@ -1,2 +1,28 @@
 #include "communication.hpp"
-
+const int capacity = 10; //JsonKeyの数
+void Communication::connectWiFi()
+{
+    WiFi.begin(wifi_ssid, wifi_password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+    }
+}
+StaticJsonDocument<JSON_OBJECT_SIZE(capacity)> Communication::post(StaticJsonDocument<JSON_OBJECT_SIZE(capacity)> json_request)
+{
+    char buffer[255];
+    serializeJson(json_request, Serial);
+    serializeJson(json_request, buffer, sizeof(buffer));
+    http.begin(url);
+    http.addHeader("Content-Type", "application/json");
+    int status_code = http.POST((uint8_t *)buffer, strlen(buffer));
+    if (status_code == 201)
+    {
+        Stream *resp = http.getStreamPtr();
+        DynamicJsonDocument json_response(255);
+        deserializeJson(json_response, *resp);
+        serializeJson(json_response, Serial);
+        return json_response;
+    }
+    http.end();
+}
--- src/communication.hpp
+++ src/communication.hpp
@@ -1,12 +1,17 @@
+#include <WiFi.cpp>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 
-class Communication{
-    private:
+extern const int capacity;
+extern char *url;
+extern char *wifi_ssid;
+extern char *wifi_password;
+
+class Communication
+{
+private:
     HTTPClient http;
-    char *url;
-    char *ssid;
-    char *password;
-    void connectWiFi();
-    void post();
+public:
+    void connectWiFi(); //WiFiに接続
+    StaticJsonDocument<JSON_OBJECT_SIZE(capacity)> post(StaticJsonDocument<JSON_OBJECT_SIZE(capacity)> json_request);  //APIをたたく
 };
