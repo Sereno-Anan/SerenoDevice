f29969f01effbee8392b82eed120b1304426e9ae
satokitakata <b-monster6-s.t.01@docomo.ne.jp>
2021-12-02 14:53:53 +0900

    #6 センサの値を取得することはできた

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,21 @@
 #include <M5StickC.h>
-#include <WiFi.h>
-#include "Wi-Fi.h"
 
-const char *ssid = wifi_ssid;
-const char *password = wifi_password;
+int PIN =36;
+int sen;
+void setup(){
+    M5.begin();
 
-int PIN = 36;
-
-void setup()
-{
-  M5.begin();
-
-  pinMode(PIN, ANALOG);
+    pinMode(PIN,ANALOG);
 }
 
-void loop()
-{
-  Serial.printf("G36: %04d\n", analogRead(PIN));
-  delay(500);
+void loop(){
+  if(analogRead(PIN)>0){
+    sen=1;
+    Serial.printf("%d\n",sen);
+    delay(300);
+  }else{
+    sen=0;
+    Serial.printf("%d\n",sen);
+    delay(3000);
+  }
 }
