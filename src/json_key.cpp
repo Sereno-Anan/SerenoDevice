c8d27eb76aad226056e8df9809fcc54eb1221a5d
KubotaShunsuke <s.kubota0921@gmail.com>
2021-12-05 22:48:12 +0900

    ファイル名変更に伴う修正(2回目) #3

--- src/json_key.cpp
+++ src/json_key.cpp
@@ -1,5 +1,5 @@
-#ifndef KEY_CPP
-#define KEY_CPP
+#ifndef JSON_KEY_CPP
+#define JSON_KEY_CPP
 const int request_key = 10;  // POSTするJsonオブジェクトのキーの数
 const int response_key = 10; // APIから返ってくるレスポンスのJsonオブジェクトのキーの数
 #endif
